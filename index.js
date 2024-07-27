e8ab8faeea30a87a82e3c23c43503c3f689af3dd
Mitchell-610 <Mheit602@gmail.com>
2024-07-27 16:53:30 -0400

    In our index file we now insert the code within the main function that will allow us to run our prompt along with getting the answers that will be held within our document file.

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const { promptUser } = require('./lib/promptUser');
 const { document } = require('./lib/document');
 
 async function main() {
-
+    try {
+        const answers = await promptUser();
+        // Call the createDocument function with the user's answers
+        document(answers);
+    } catch (error) {
+        console.error('Error during prompt or document creation:', error);
+    }
 }
 
 main();
