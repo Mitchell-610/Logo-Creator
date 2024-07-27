66ddc51e10763a4f32f9b3f7c1c02ef6649dc718
Mitchell-610 <Mheit602@gmail.com>
2024-07-27 17:18:58 -0400

    Testing setup for triangle.

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -10,4 +10,14 @@ describe('Shape Classes', () => {
         expect(square.render().trim()).toEqual(expectedSvg.trim());
     });
 
+
+    test('Triangle render method should return correct SVG', () => {
+        const triangle = new Triangle('Test', 'red', 'blue');
+        const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+  <polygon points="150, 18 244, 182 56, 182" fill="blue" />
+  <text x="150" y="150" font-size="60" text-anchor="middle" fill="red">Test</text>
+</svg>`;
+        expect(triangle.render().trim()).toEqual(expectedSvg.trim());
+    });
+
 });
