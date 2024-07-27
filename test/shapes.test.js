83f33ee111fcf26579e5f24d5de18503b47b4fa4
Mitchell-610 <Mheit602@gmail.com>
2024-07-27 17:18:26 -0400

    We setup all of our testing for a square.

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,5 +1,13 @@
 const { Square, Circle, Triangle } = require('../lib/shapes');
 
 describe('Shape Classes', () => {
-   
+    test('Square render method should return correct SVG', () => {
+        const square = new Square('Test', 'red', 'blue');
+        const expectedSvg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <rect x="90" y="40" width="120" height="120" fill="blue" />
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="red">Test</text>
+      </svg>`;
+        expect(square.render().trim()).toEqual(expectedSvg.trim());
+    });
+
 });
